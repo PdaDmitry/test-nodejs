5e2df82a756a228a4deecc52f1091e76ce1093c7
PdaDmitry <pda.gazcomp@gmail.com>
2024-09-01 19:13:01 +0300

    res.status()

--- src/index.js
+++ src/index.js
@@ -1,11 +1,55 @@
+// import express from 'express';
+// import req from 'express/lib/request';
+// import res from 'express/lib/response';
+
+// const PORT = 3000;
+// const app = express();
+
+// app.get('/', (req, res) => {
+//   res.json({
+//     message: 'Hello world!!!',
+//   });
+// });
+
+// app.use((req, res, next) => {
+//   console.log(`Time: ${new Date().toLocaleString()}`);
+//   next();
+// });
+
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
+// ============================================================
+
+// src/index.js
+
 import express from 'express';
-const app = express();
 
 const PORT = 3000;
 
+const app = express();
+
+// Middleware для логування часу запиту
+app.use((req, res, next) => {
+  console.log(`Time: ${new Date().toLocaleString()}`);
+  next();
+});
+
+// Вбудований у express middleware для обробки (парсингу) JSON-даних у запитах
+// наприклад, у запитах POST або PATCH
+app.use(express.json());
+
+// Маршрут для обробки GET-запитів на '/'
 app.get('/', (req, res) => {
   res.json({
-    message: 'Hello world!!!',
+    message: 'Hello, World!',
+  });
+});
+
+// Middleware для обробких помилок (приймає 4 аргументи)
+app.use((err, req, res, next) => {
+  res.status(500).json({
+    message: 'Something went wrong',
   });
 });
 
