897b40d6a8b20c51289e8bee60962c3bf52a4603
PdaDmitry <146446934+PdaDmitry@users.noreply.github.com>
2024-09-08 14:07:02 +0300

    console.log(defaultValue);

--- src/utils/env.js
+++ src/utils/env.js
@@ -6,8 +6,10 @@ export function env(name, defaultValue) {
   const value = process.env[name];
   if (value) return value;
   console.log(value);
+  console.log(value);
 
   if (defaultValue) return defaultValue;
+  console.log(defaultValue);
   console.log(defaultValue);
   throw new Error(`Missing: process.env['${name}'].`);
 }
